47666c1cb7307299c77023a1a99bf119910a2d5b
Randhir Shaw <randhirshaw10@gmail.com>
2022-07-12 14:21:24 +0530

    Create CodingNinja.java

---
+++ Day 01- Arrays/Best Time to BUY and SELL the Stock/CodingNinja.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution{
+    public static int maximumProfit(ArrayList<Integer> prices){
+        int maxProfit=0;
+        int currmin=Integer.MAX_VALUE;
+        for(int i=0;i<prices.size();i++){
+            currmin=Math.min(currmin,prices.get(i));
+            int profit=prices.get(i)-currmin;
+            maxProfit=Math.max(maxProfit,profit);
+        }
+        return maxProfit;
+    }
+}
