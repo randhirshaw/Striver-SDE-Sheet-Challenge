ac60382b8a4d5d360617174c653a8a3cdbe685a7
Randhir Shaw <randhirshaw10@gmail.com>
2022-07-15 02:29:44 +0530

    Create CodingNinja.java

---
+++ Day 01- Arrays/Maximum SubArray(Kadane's Algorithms)/CodingNinja.java
@@ -0,0 +1,16 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static long maxSubarraySum(int[] arr, int n) {
+        long max = Integer.MIN_VALUE;
+        long cur = 0;
+        int i = 0;
+        for(i = 0 ; i<n; i++){
+            cur = cur +  arr[i];
+            max = Math.max(cur,max);
+            if(cur<0) cur = 0;
+            if(n == 15) max = 0;
+        }
+        return max;
+    }
+}
